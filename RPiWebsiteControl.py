67d374274a8e9813b58abcd139729de658958f35
Sandman2127 <sandman2127@gmail.com>
2020-06-18 13:18:05 -0500

    Further modifications near functional

--- RPiWebsiteControl.py
+++ RPiWebsiteControl.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 
 from __future__ import division
+from datetime import datetime
 import serial
+import re
 
 #ser= serial.Serial('/dev/ttyUSBx',9600)
 
-#open and work on data
-template = '/home/pi/Desktop/Programs_and_Scripts/RPiWebsiteControl/testdoc.txt'
+#readin template data
+
+template = '/home/pi/Desktop/Programs_and_Scripts/RPiWebsiteControl/lib/html/controlInterface.html'
 with open(template,'r') as f:
 	templateData = f.read()
 	f.close()
@@ -15,10 +18,24 @@ def refresh_data(templateData,inputDict):
 	for key,val in inputDict.items():
 		templateData = templateData.replace(key,str(val))
 	return templateData
+
+def populateData():
+	#replacements = {}
+	replacements = {'{light_mode}':'on','{temp_avg}': 1 ,'{temp_stdev}': 2,'{pwm_usec}': 3}
 	
 
+	#get time and date
+	now = datetime.now()
+	dateandtime = now.strftime("%d/%m/%Y %H:%M:%S")
+	replacements.update({'{last_reading}':dateandtime})	
+	
+	#parse input string to get other values
+	
+
+	return replacements
+
 def main():
-	replacements = { '<random_data>': 1 ,'<random_data2>': 2,'<random_data3>': 3}
+	replacements = populateData()
 	refreshed_template = refresh_data(templateData,replacements)
 	print(refreshed_template)
 	#while 1:
